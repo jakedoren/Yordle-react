a9bdf45e0f37e749d37e7b2f572909d945fc90ea
Jacob Doren <Jacob Doren>
2022-03-05 12:07:22 -0500

    cleanup tests

--- src/tests/Keyboard.test.js
+++ src/tests/Keyboard.test.js
@@ -1,12 +1,7 @@
-import { fireEvent, getAllByRole, render } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
-import axios from "axios"
 import { BrowserRouter } from "react-router-dom"
-import { isConstructorDeclaration } from "typescript"
-import App from "../App"
-import Keyboard from "../components/Keyboard"
 import Wordle from "../components/Wordle"
-import { setCookie } from "../helpers/helper"
 
 it('enters character on the screen', async () => {
     const { debug, container } = render(<BrowserRouter><Wordle /></BrowserRouter>)
