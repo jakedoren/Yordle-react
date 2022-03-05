0cf59ca4d6206411e2616f2f9c2aeaf23214adf7
Jacob Doren <Jacob Doren>
2022-03-05 12:06:55 -0500

    full wordle flow test

--- src/tests/Keyboard.test.js
+++ src/tests/Keyboard.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { fireEvent, getAllByRole, render } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
 import axios from "axios"
 import { BrowserRouter } from "react-router-dom"
@@ -25,5 +25,40 @@ it('enters a five letter word with the keyboard', () => {
     helloArray.forEach((char, i) => {
         expect(firstRowChildren[i].innerHTML).toBe(char)
     })
-    debug()
+})
+
+it('enters a guess and continues to the next line', () => {
+    const { debug, container } = render(<BrowserRouter><Wordle /></BrowserRouter>)
+    userEvent.keyboard('hello')
+    const helloArray = ['h', 'e', 'l', 'l', 'o']
+    const firstRow = container.getElementsByClassName
+    ('grid-container')[0]
+    const firstRowChildren = firstRow.getElementsByTagName('div')
+    helloArray.forEach((char, i) => {
+        expect(firstRowChildren[i].innerHTML).toBe(char)
+    })
+    const enterBtn = container.getElementsByTagName('button')[0]
+    userEvent.click(enterBtn)
+    userEvent.keyboard('flask')
+    const flaskArray = ['f', 'l', 'a', 's', 'k']
+    const secondRow = container.getElementsByClassName('grid-container')[1]
+    const secondRowChildren = secondRow.getElementsByTagName('div')
+    flaskArray.forEach((char, i) => {
+        expect(secondRowChildren[i].innerHTML).toBe(char)
+    })
+})
+
+it('goes through the wordle guess flow and maps all characters to their respective grid box', () => {
+    const { debug, container } = render(<BrowserRouter><Wordle /></BrowserRouter>)
+    const gridContainers = container.getElementsByClassName('grid-container')
+    const enterBtn = container.getElementsByTagName('button')[0]
+    for(let i = 0; i < gridContainers.length; i++) {
+        const childDivs = gridContainers[i].getElementsByTagName('div')
+        userEvent.keyboard('flask')
+        const guessArray = ['f', 'l', 'a', 's', 'k']
+        guessArray.forEach((char, i) => {
+            expect(childDivs[i].innerHTML).toBe(char)
+        })
+        userEvent.click(enterBtn)
+    }
 })
