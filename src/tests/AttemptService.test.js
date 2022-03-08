4ddd14de090e1550aa4de8e47cfa6ebd10a357a2
Jacob Doren <jakedoren@gmail.com>
2022-03-07 21:51:44 -0500

    test color of matches

--- src/tests/AttemptService.test.js
+++ src/tests/AttemptService.test.js
@@ -1,5 +1,8 @@
+import { render, waitFor } from '@testing-library/react'
 import { validateAttemptService } from '../helpers/requests'
 import { server, rest } from '../testServer'
+import Wordle from '../components/Wordle'
+import userEvent from '@testing-library/user-event'
 
 it('sends back guess', async () => {
     const res = await validateAttemptService('betty')
@@ -22,4 +25,29 @@ it ('handles failure', async () => {
         expect(e.response.status).toBe(500)
         expect(e.response.data).toStrictEqual({ error: "401 unauthorized" })
     }
+})
+
+describe('it validates guess, and changes color of keys in guess according to exact and partial matches', () => {
+    it('matches characters', async () => {
+        const { debug, container } = render(<Wordle />)
+        userEvent.keyboard('hello')
+        const helloArray = ['h', 'e', 'l', 'l', 'o']
+        const firstRow = container.getElementsByClassName
+        ('grid-container')[0]
+        const firstRowChildren = firstRow.getElementsByTagName('div')
+        helloArray.forEach((char, i) => {
+            expect(firstRowChildren[i].innerHTML).toBe(char)
+        })
+        const enterBtn = container.getElementsByTagName('button')[0]
+        userEvent.click(enterBtn)
+        const partialMatches = [firstRowChildren[0], firstRowChildren[3]]
+        const exactMactches = [firstRowChildren[1], firstRowChildren[2]]
+        await waitFor(() => expect(partialMatches[0].classList.contains('yellow')).toBe(true))
+        partialMatches.forEach(match => {
+            expect(match.classList.contains('yellow')).toBe(true)
+        })
+        exactMactches.forEach(match => {
+            expect(match.classList.contains('green')).toBe(true)
+        })
+    })
 })
